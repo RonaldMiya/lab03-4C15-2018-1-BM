732f41ec4a025271a506211343b82c9081d11818
RonaldMiya <ronald.miya@tecsup.edu.pe>
2018-02-26 09:51:57 -0500

    First commit

---
+++ src/main/java/com/bm/tecsup/lab03/lab03_4C15_2018_1_BM/App.java
@@ -0,0 +1,13 @@
+package com.bm.tecsup.lab03.lab03_4C15_2018_1_BM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

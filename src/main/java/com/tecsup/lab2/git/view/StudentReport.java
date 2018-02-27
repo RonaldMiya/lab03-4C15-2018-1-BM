a21cdb95089f8a831e454cd10fb0297bc4adc941
jhosepv13 <jhosepv13@hotmail.com>
2018-02-26 21:03:56 -0500

    "Saltos de Linea"

--- src/main/java/com/tecsup/lab2/git/view/StudentReport.java
+++ src/main/java/com/tecsup/lab2/git/view/StudentReport.java
@@ -1,5 +1,24 @@
 package com.tecsup.lab2.git.view;
 
+import javax.swing.JOptionPane;
+
 public class StudentReport {
+	
+	public static void main(String []args) {
+		
+
+		String nombre, apellido, ciclo;
+		
+		nombre = JOptionPane.showInputDialog("ingrese el nombre"); 
+		apellido = JOptionPane.showInputDialog("ingrese el apellido");
+		ciclo = JOptionPane.showInputDialog("ingrese el ciclo");
+		
+		
+		JOptionPane.showMessageDialog(null, "Resultados:" + "/t" + nombre +
+										"/t" + apellido +
+										"/t" + ciclo);
+		
+		
+	}
 
 }
